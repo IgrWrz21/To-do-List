c36bdaab3917f758f2ee361d2c73b5d9b9389ec9
unknown <igorwrzesinski22@wp.pl>
2024-03-13 22:45:31 +0100

    deleteTaskModule moved to Task class

--- toDoApp/assets/scripts/app.js
+++ toDoApp/assets/scripts/app.js
@@ -4,14 +4,19 @@ const mainSectionNode = document.querySelector("main");
 const taskTemplate = document.getElementById("taskTemplate");
 const popupTemplate = document.getElementById("popupTemplate");
 
+let taskNumber = 0;
+
 let taskId = 0;
 
 class Task {
-  constructor(id) {
+  constructor(id, pgrsBar) {
     this.taskContent = textAreaNode.value;
     this.isActive = false;
     this.id = `cbx-${id}`;
     this.acceptButton;
+    this.progressBar = pgrsBar;
+    //console.log(this.progressBar);
+    taskNumber++;
   }
 
   creatTaskModule() {
@@ -21,8 +26,8 @@ class Task {
 
     clone.querySelector(".taskContent p").textContent = this.taskContent;
     clone.querySelector("label").htmlFor = this.id;
-
-    this.setLisnerForClose(clone.querySelector(".fa-xmark"));
+    this.button = clone.querySelector(".fa-xmark");
+    this.setEvtListenerForPoppUp(this.button);
 
     checkbox.addEventListener("change", () => {
       this.isActive = checkbox.checked;
@@ -31,64 +36,102 @@ class Task {
     });
     mainSectionNode.prepend(clone);
   }
-  deleteTaskModal(evt) {
-    let target = evt.parentElement;
+
+  setEvtListenerForPoppUp(button) {
+    console.log(button);
+    button.addEventListener("click", (button) => {
+      console.log(this, "to to");
+      new PopUp(button.srcElement, this.deleteTaskModule);
+    });
+  }
+  deleteTaskModule() {
+    let target = this.button.parentElement;
     target.classList.remove("apearAnimation");
     target.classList.add("disapearAnimation");
 
     setTimeout(() => {
       target.remove();
+      taskNumber--;
+      //updateProgresBar(activeTaskNumber,taskNumber)
     }, 300);
   }
+}
 
-  createModal(button) {
+class PopUp {
+  constructor(button, deleteFuntion) {
     const clone = popupTemplate.content.cloneNode(true);
     document.body.prepend(clone);
     document.body.classList.add("stopScroll");
-
-    const popup = document.querySelector(".popup");
-    const backDrop = document.querySelector(".bgc");
-    const denaidBtn = popup.querySelector("#deinedButton");
-    const acceptButton = popup.querySelector("#acceptButton");
-    this.setLisnersForPopup(popup, backDrop, denaidBtn, acceptButton, button);
+    this.popup = document.querySelector(".popup");
+    this.backDrop = document.querySelector(".bgc");
+    this.deniedBtn = this.popup.querySelector("#deinedButton");
+    this.acceptButton = this.popup.querySelector("#acceptButton");
+    this.button = button;
+    this.deleteTaskModuleFunction = deleteFuntion;
+    this.setLisnersForPopup();
   }
 
-  setLisnerForClose(button) {
-    button.addEventListener("click", this.createModal.bind(this, button));
-  }
-
-  setLisnersForPopup(popup, backDrop, denaidBtn, acceptButton, button) {
-    denaidBtn.addEventListener("click", () => {
-      popup.classList.add("disapearAnimation");
-      setTimeout(() => {
-        popup.remove();
-        backDrop.remove();
-        document.body.classList.remove("stopScroll");
-      }, 300);
+  setLisnersForPopup() {
+    this.deniedBtn.addEventListener("click", () => {
+      this.popup.classList.add("disapearAnimation");
+      this.popUpRemover(300);
     });
-    acceptButton.addEventListener("click", () => {
-      this.deleteTaskModal(button);
-      //popup.classList.add("disapearAnimation");
-      setTimeout(() => {
-        popup.remove();
-        backDrop.remove();
-        document.body.classList.remove("stopScroll");
-      });
+    this.acceptButton.addEventListener("click", () => {
+      this.deleteTaskModuleFunction(this.button);
+      this.popUpRemover(0);
     });
   }
-}
-class ListInit {
-  static initTask() {
-    if (textAreaNode.value === "") {
-      alert("Add someting to the input");
-      return;
-    }
-    let t1 = new Task(taskId);
+  popUpRemover(time) {
+    setTimeout(() => {
+      this.popup.remove();
+      this.backDrop.remove();
+      document.body.classList.remove("stopScroll");
+    }, time);
+  }
+  // deleteTaskModule(evt) {
+  //   let target = evt.parentElement;
+  //   target.classList.remove("apearAnimation");
+  //   target.classList.add("disapearAnimation");
 
-    t1.creatTaskModule();
-    taskId++;
-    textAreaNode.value = "";
+  //   setTimeout(() => {
+  //     target.remove();
+  //     taskNumber--;
+  //     //updateProgresBar(activeTaskNumber,taskNumber)
+  //   }, 300);
+  // }
+}
+
+class appInit {
+  static init() {
+    let b1 = new ProgressBar();
+    b1.updateProgresBar(5, 10);
+
+    addTaskButton.addEventListener("click", () => {
+      //console.log(b1);
+      if (textAreaNode.value === "") {
+        alert("Add someting to the input");
+        return;
+      }
+      let t1 = new Task(taskId);
+      t1.creatTaskModule();
+      taskId++;
+      textAreaNode.value = "";
+    });
+  }
+  //static init;
+}
+
+class ProgressBar {
+  constructor() {
+    this.progressBarDiv = document.querySelector(".bar");
+  }
+
+  updateProgresBar(activeTasks, allTaskNumber) {
+    const lvl = (activeTasks / allTaskNumber) * 100;
+    this.progressBarDiv.style.width = `${lvl}%`;
+    this.progressBarDiv.textContent = `${lvl.toFixed(1)}%`;
   }
 }
 
-addTaskButton.addEventListener("click", ListInit.initTask);
+appInit.init();
+//addTaskButton.addEventListener("click", appInit.init);
